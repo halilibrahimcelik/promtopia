060e902e74d41acdf101df9a22d9a0225d7f5e60
halilibrahimcelik <halil.celik139@gmail.com>
2023-05-18 18:06:17 +0300

    feat: navbar component added.

--- app/page.js
+++ app/page.js
@@ -1,3 +1,20 @@
+import Feed from "@components/Feed";
+
 export default function Home() {
-  return <main className="">hello</main>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="text-center orange_gradient">AI-powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam qui
+        dolor debitis quis explicabo aut architecto aspernatur odit animi
+        itaque?
+      </p>
+
+      <Feed />
+    </section>
+  );
 }
