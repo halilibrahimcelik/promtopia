8755299d3b1df756f4392e6fb773af12737c181f
halilibrahimcelik <halil.celik139@gmail.com>
2023-05-18 16:45:18 +0300

    added project assets and needed tools

--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-export default function Hom() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
+export default function Home() {
+  return <main className="">hello</main>;
 }
