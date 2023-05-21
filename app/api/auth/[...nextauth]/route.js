2cb7e068a411ee548d1b876a7c519aeb95f20942
halilibrahimcelik <halil.celik139@gmail.com>
2023-05-21 10:30:24 +0300

    feat: backend auth database added.

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+console.log({
+  clientId: process.env.GOOGLE_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+});
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  async session(session, token) {},
+  async signIn({ proifle }) {
+    try {
+      //servless route => Lambda function => serverless function
+    } catch (error) {}
+  },
+});
+
+export { handler as GET, handler as POST };
