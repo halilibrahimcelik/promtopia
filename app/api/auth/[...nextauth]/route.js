169920e3889c2d13026951da3af3a0215b14e8f2
halilibrahimcelik <halil.celik139@gmail.com>
2023-05-21 10:56:24 +0300

    auth process added

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-console.log({
-  clientId: process.env.GOOGLE_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-});
+import { connectToDB } from "@utils/mongodb";
+
+import User from "@models/user";
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -11,11 +10,30 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session(session, token) {},
-  async signIn({ proifle }) {
+  async session(session) {
+    const sessionUser = await User.findOne({ email: session.user.email });
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
+  async signIn({ profile }) {
     try {
       //servless route => Lambda function => serverless function
-    } catch (error) {}
+      await connectToDB();
+      //check if user exists in db
+      const userExist = await User.findOne({ email: profile.email });
+      //if not, create user
+      if (!userExist) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   },
 });
 
